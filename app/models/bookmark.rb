b0052338e73e252f4cfdb3968f2f51180f2232ef
Henrique Langnas <henri.slang@gmail.com>
2022-03-25 20:13:40 +0000

    fixing

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :list_id
   belongs_to :movie_id
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
 end
