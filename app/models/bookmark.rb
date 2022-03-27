544732bd20f26d2162fb8bce429b6d00eaec95e8
Henrique Langnas <henri.slang@gmail.com>
2022-03-27 17:32:58 +0100

    again

--- app/models/bookmark.rb
+++
@@ -1,7 +0,0 @@
-class Bookmark < ApplicationRecord
-  belongs_to :list_id
-  belongs_to :movie_id
-
-  validates :comment, length: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
-end
